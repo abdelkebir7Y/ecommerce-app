e855ff0fa086fd82acb07faa3bf3be5e10cbc9bf
abdelkebir7Y <abdouhassou92@gmail.com>
2021-03-06 23:35:16 +0100

    adding shop page & collection preview component & collection item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,8 +48,8 @@ class Directory extends React.Component {
     return (
       <div className='directory-menu'>
         {
-          this.state.sections.map(({title , id , imageUrl ,size }) => (
-            <MenuItem title={title.toUpperCase()} key={id} imageUrl={imageUrl} size = {size} />
+          this.state.sections.map(({id , ...otherSectionProps }) => (
+            <MenuItem key={id} {...otherSectionProps} />
             )
           )
         }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,10 +3,12 @@ import Directory from '../../components/directory/directory.component';
 
 import './homepage.styles.scss';
 
+
+
 const HomePage = () => {
     return (
         <div className='homepage'>
-            <Directory />
+            <Directory/>        
         </div>
     );
 }
